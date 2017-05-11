40481757e77b5e54e062dddc7b7ecedfd72f30e8
currymich <currymich@gmail.com>
2017-05-11 15:58:55 -0700

    added route to point to email submission route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  post '/users/login', to: 'users#login'
-  post '/users/signup', to: 'users#signup'
+  resources :users, only: [:show] do
+    collection do
+      post '/login', to: 'users#login'
+      post '/signup', to: 'users#signup'
+    end
+  end
 
+  resources :questions, only: [:create]
 end
