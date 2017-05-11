2912dcb57708eee5666f1a8bb2e6db56b58b759f
currymich <currymich@gmail.com>
2017-05-11 16:01:52 -0700

    route to send email with question to properguide email

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def create
+    question = Question.new(question_params)
+
+    if question.save!
+      QuestionMailer.question_email(question).deliver_later
+      render json: {status: 201, question: question}
+    else
+      render json: {status: 422}
+    end
+  end
+
+  private
+
+    def question_params
+      params.require(:question).permit(:subject, :text, :phone, :email, :name)
+    end
+end
