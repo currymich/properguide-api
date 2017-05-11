25bb51d9f07675f4505b6685bf8de55db2dbe124
currymich <currymich@gmail.com>
2017-05-10 17:38:09 -0700

    pulled user routes out of user resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users do
-    collection do
-      post '/login', to: 'auth#login'
-      post '/signup', to 'auth#signup'
-    end
-  end
+
+  post '/users/login', to: 'users#login'
+  post '/users/signup', to: 'users#signup'
+
 end
