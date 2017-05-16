37bcd10c27206b762ad9dd599fbaf67e3d06dbed
currymich <currymich@gmail.com>
2017-05-16 15:28:40 -0700

    creating blank controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :order_items, only: [:create, :update, :destroy, :index]
+
+  resources :carts, only: [:show]
+
+  resources :products, only: [:index, :show]
+
   resources :users, only: [:show] do
     collection do
       post '/login', to: 'users#login'
