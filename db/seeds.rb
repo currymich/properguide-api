ee440849fa10aacfeb16eccb2f61e812a0ea69fc
currymich <currymich@gmail.com>
2017-05-19 14:58:41 -0700

    adding more products to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,5 +13,14 @@ OrderStatus.create! id: 3, name: "Shipped"
 OrderStatus.create! id: 4, name: "Cancelled"
 
 Product.destroy_all
-Product.create! id: 1, name: "Surgical Stent", price: 300, image_url: "../img/stent_guide.jpg", active: true
-Product.create! id: 2, name: "Crown", price: 500, image_url: "../img/case3.jpg", active: true
+Product.create! id: 1, name: "Surgical Stent", price: 120, image_url: "../img/stent_guide.jpg", alt_image: "../img/stent_guide.jpg", active: true
+
+Product.create! id: 2, name: "Crown", price: 150, image_url: "../img/crown1.jpg", alt_image: "../img/crown2.jpg", active: true
+
+Product.create! id: 3, name: "Veneer", price: 150, image_url: "../img/veneer1.jpg", alt_image: "../img/veneer2.jpg", active: true
+
+Product.create! id: 4, name: "Screw Retained", price: 400, image_url: "../img/screw1.jpg", alt_image: "../img/screw2.jpg", active: true
+
+Product.create! id: 5, name: "Cement Retained", price: 400, image_url: "../img/cement1.jpg", alt_image: "../img/cement2.jpg", active: true
+
+Product.create! id: 6, name: "Bridge", price: 150, image_url: "../img/bridge1.jpg", alt_image: "../img/bridge2.jpg", active: true
