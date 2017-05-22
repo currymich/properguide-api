d18587634a2c8476fe8002e16ac5c251c3a0ce7b
currymich <currymich@gmail.com>
2017-05-22 11:20:43 -0700

    renaming controller and adding error message to missing orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,15 @@
-class OrderController < ApplicationController
+class OrdersController < ApplicationController
+  before_action :authenticate
+
   def index
     render json: {orders: Order.all}
   end
 
   def show
     render json: {order: Order.find(params[:id])}
+
+    rescue ActiveRecord::RecordNotFound => error
+      render json: {error: "Order not found"}
   end
 
   def create
@@ -16,4 +21,10 @@ class OrderController < ApplicationController
       render json: {status: 422}
     end
   end
+
+  private
+
+    def order_params
+      params.required(:order).permit(:order_status_id, :dentist_id, :patient_name)
+    end
 end
