4090915adbe009de5bb7472cb960c94482d42bf3
currymich <currymich@gmail.com>
2017-05-22 10:17:20 -0700

    filling out new routes for order controller

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,3 +1,19 @@
 class OrderController < ApplicationController
-  
+  def index
+    render json: {orders: Order.all}
+  end
+
+  def show
+    render json: {order: Order.find(params[:id])}
+  end
+
+  def create
+    @order = Order.new(order_params)
+
+    if @order.save!
+      render json: {status: 201, order: @order}
+    else
+      render json: {status: 422}
+    end
+  end
 end
