412d21e09a2f10ed10199b83b47b2ab37de58577
currymich <currymich@gmail.com>
2017-05-25 11:43:44 -0700

    removing extra end

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,5 @@ module ProperguideApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-   end
   end
 end
