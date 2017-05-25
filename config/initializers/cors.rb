c051326ff109c1865425159bade8b9845c14a185
currymich <currymich@gmail.com>
2017-05-25 12:10:08 -0700

    updating cors regex, attempt 10

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /properguide-api\.herokuapp\.com/, /properguideimplant(s*)\.com/
+    origins /(http[s]*:\/\/)?properguide-(api|ui).herokuapp.com/, /(http[s]*:\/\/)?(www\.)?properguideimplant[s]?\.com/
 
     resource '*',
       headers: :any,
