840fda71b123d4a11ea33ea1485c7a5022417027
currymich <currymich@gmail.com>
2017-06-06 14:54:24 -0700

    create and show routes built - create generates new dentist, then new user

--- app/controllers/dentists_controller.rb
+++ app/controllers/dentists_controller.rb
@@ -1,15 +1,36 @@
 class DentistsController < ApplicationController
   #require current user to be admin
   before_action :authenticate, except: [:show]
-  
+
   # require current user id to match id on request
   before_action :authorize, except: [:create]
 
   def create
+    @dentist = Dentist.new(dentist_params)
 
+    if @dentist.save!
+      @user = User.new(name: params[:dentist][:name], username: params[:dentist][:email], email: params[:dentist][:email], password: "temp_pass", dentist_id: @dentist.id)
+      if @user.save!
+        render json: {status: 201, dentist: @dentist, user: @user}
+      end
+    else
+      render json: {status: 422}
+    end
   end
 
   def show
     # return dentist info, current orders
+    @dentist = Dentist.find(params[:id])
+    @orders = @dentist.orders
+
+    render json: {dentist: @dentist, orders: @orders}
+  rescue ActiveRecord::RecordNotFound => error
+    render json: {error: "Dentist not found"}
   end
+
+  private
+
+    def dentist_params
+      params.required(:dentist).permit(:name, :office_name, :email, :phone, :address, :address_state, :address_zip, :address_city, :license_num)
+    end
 end
