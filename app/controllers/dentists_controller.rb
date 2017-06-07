86e00e76f537a6e1d44f09fb8f2083dee21ba990
currymich <currymich@gmail.com>
2017-06-07 13:17:00 -0700

    add code to log in dentist on signup

--- app/controllers/dentists_controller.rb
+++ app/controllers/dentists_controller.rb
@@ -10,8 +10,12 @@ class DentistsController < ApplicationController
 
     if @dentist.save!
       @user = User.new(name: params[:dentist][:name], username: params[:dentist][:email], email: params[:dentist][:email], password: "temp_pass", dentist_id: @dentist.id)
+
       if @user.save!
-        render json: {status: 201, dentist: @dentist, user: @user}
+        @user.authenticate(params[:dentist][:password])
+        @token = token(@user.id, @user.email)
+
+        render json: {message: "user created and logged in", status: 201, dentist: @dentist, user: @user, token: @token}
       end
     else
       render json: {status: 422}
