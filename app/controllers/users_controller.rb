c5bd740b474c729ee65a045710b12aef82e0eb46
currymich <currymich@gmail.com>
2017-06-13 14:11:12 -0700

    added user update route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+
   def login
     if params[:user][:email].include? "@"
       @user = User.find_by(email: params[:user][:email])
@@ -13,6 +13,18 @@ class UsersController < ApplicationController
       render json: {status: 201, user: @user, message: "user logged in", token: @token}
     else
       render json: {status: 401, message: "unauthorized"}
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    render json: {status: 401, message: "unauthorized"} unless current_user && (current_user.id == @user.id || current_user.admin?)
+
+    if @user.update!(user_params)
+      render json:{status: 200, message: "User updated", user: @user}
+    else
+      render json:{status: 422, message: "Couldn't process updated params"}
     end
   end
 
