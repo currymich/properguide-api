7400814f5d602c7082409c5e3e67182a885672ce
currymich <currymich@gmail.com>
2017-07-06 16:39:08 -0700

    adding newly created payment to response

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,9 +1,9 @@
 class PaymentsController < ApplicationController
   def create
     @order = Order.find(params[:order_id])
-
-    if @order.payments.create!(payment_params)
-      render json: {status: 201, payments: Payment.all, order: @order}
+    @payment = @order.payments.new!(payment_params)
+    if @payment.save!
+      render json: {status: 201, payments: Payment.all, order: @order, payment: @payment}
     else
       render json: {status: 422}
     end
