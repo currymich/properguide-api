1f7807066cdf39a0f3a2a43154192ce5f17ef3f3
currymich <currymich@gmail.com>
2017-07-18 11:46:38 -0700

    making log more visible

--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -40,7 +40,9 @@ class BraintreeController < ApplicationController
       @order = Order.find(params[:order_id])
       @dentist = @order.dentist
 
-      puts @order
+      puts "_________________________\n
+            #{@order.pay_due}\n
+            __________________________"
 
       result = Braintree::Transaction.sale(
         :customer_id => @dentist.id,
