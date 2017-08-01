ebeb891f29a51c3d1c7155837f347a4d44974013
currymich <currymich@gmail.com>
2017-08-01 09:35:13 -0700

    Adding 'all orders' to the response json from order update route

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -52,7 +52,7 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
 
     if @order.update(order_params)
-      render json:{status: 200, message: "Order updated", order: @order}
+      render json:{status: 200, message: "Order updated", order: @order, orders: Order.all}
     else
       render json:{status: 422, message: "Couldn't process updated params"}
     end
