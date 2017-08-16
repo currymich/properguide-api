5ccf82817735d495f960803e3ce14204fe0bd206
currymich <currymich@gmail.com>
2017-08-15 17:42:00 -0700

    fixing if syntax

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -19,9 +19,9 @@ class OrderItemsController < ApplicationController
 
       if @order.order_items.create!(order_item_params)
         # If there is a metal fee, add the metal fee product also
-        if (Product.find(params[:product_id]).extra_fee) {
+        if (Product.find(params[:product_id]).extra_fee)
           @order.order_items.create!(product_id: 10, quantity: 0);
-        }
+        end
         render json: {status: 201, order_items: @order.order_items, order: @order}
       else
         render json: {status: 422}
